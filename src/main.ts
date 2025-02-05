fd3b3b1b08da2729e22c6032a053e15a5ce52d0e
francoragout <francoragout1@gmail.com>
2025-02-04 23:37:07 -0300

    work on users CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('SchoolSync API')
+    .setDescription('The SchoolSync API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,28 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
 
+export class UserEntity implements User {
+  @ApiProperty()
+  id: string;
 
+  @ApiProperty()
+  firstName: string;
+
+  @ApiProperty()
+  lastName: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  emailVerified: Date | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  image: string | null;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
