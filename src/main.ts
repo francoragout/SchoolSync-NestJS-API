62dbd5bd9f576c95a11533e8445bca52d22f8abf
francoragout <francoragout1@gmail.com>
2025-03-06 00:12:13 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'https://school-sync-five.vercel.app/',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.setGlobalPrefix('api');
